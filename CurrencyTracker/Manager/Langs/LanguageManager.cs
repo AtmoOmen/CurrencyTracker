47b693768ebacb441d95720d59a3414e71e02eeb
AtmoOmen <atmoomen2004@outlook.com>
2023-12-28 18:19:27 +0800

    Probole Fix

--- CurrencyTracker/Manager/Langs/LanguageManager.cs
+++ CurrencyTracker/Manager/Langs/LanguageManager.cs
@@ -1,5 +1,3 @@
-using System.Net.Http;
-
 namespace CurrencyTracker.Manager;
 
 public class LanguageManager
@@ -128,58 +126,6 @@ public class LanguageManager
         ssb.AddText(format.Substring(lastIndex));
 
         return ssb.Build();
-    }
-}
-
-public class LanguageUpdater()
-{
-    public static async Task DownloadLanguageFilesAsync()
-    {
-        var httpClient = new HttpClient();
-        var baseUrl = "https://raw.githubusercontent.com/AtmoOmen/CurrencyTracker/master/CurrencyTracker/Manager/Langs/";
-        var fallbackBaseUrl = "https://raw.githubusercontents.com/AtmoOmen/CurrencyTracker/master/CurrencyTracker/Manager/Langs/";
-
-        if (!Directory.Exists(LanguageManager.LangsDirectory))
-        {
-            Directory.CreateDirectory(LanguageManager.LangsDirectory);
-        }
-
-        foreach (var language in LanguageManager.LanguageNames)
-        {
-            var success = await TryDownloadLanguageFileAsync(httpClient, baseUrl, language.Language);
-            if (!success)
-            {
-                await TryDownloadLanguageFileAsync(httpClient, fallbackBaseUrl, language.Language);
-            }
-        }
-    }
-
-    private static async Task<bool> TryDownloadLanguageFileAsync(HttpClient httpClient, string baseUrl, string language)
-    {
-        var url = $"{baseUrl}{language}.resx";
-        var localFilePath = Path.Combine(LanguageManager.LangsDirectory, $"{language}.resx");
-
-        try
-        {
-            var response = await httpClient.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-            {
-                var content = await response.Content.ReadAsByteArrayAsync();
-                await File.WriteAllBytesAsync(localFilePath, content);
-                Service.Log.Debug($"Successfully downloaded {language} language file.");
-                return true;
-            }
-            else
-            {
-                Service.Log.Error($"Failed to download {language} language file. Status: {response.StatusCode}");
-                return false;
-            }
-        }
-        catch (Exception ex)
-        {
-            Service.Log.Error($"Error downloading {language} language file. Error: {ex.Message}");
-            return false;
-        }
     }
 }
 
