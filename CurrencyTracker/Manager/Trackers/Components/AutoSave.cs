cf98c0c6dcf5e46cd9a967958be81a4d75abc5c4
AtmoOmen <atmoomen2004@outlook.com>
2024-03-08 22:47:33 +0800

    Try fix compile error

--- CurrencyTracker/Manager/Trackers/Components/AutoSave.cs
+++ CurrencyTracker/Manager/Trackers/Components/AutoSave.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Timers;
 using CurrencyTracker.Manager.Infos;
 using CurrencyTracker.Manager.Transactions;
-using static CurrencyTracker.Plugin;
 
 namespace CurrencyTracker.Manager.Trackers.Components;
 
